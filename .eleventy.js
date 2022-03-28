f8b29a569ecdc17383713ff2a48d2c77caedcb84
Mike Aparicio <peruvianidol@gmail.com>
2022-03-28 10:13:21 -0500

    Remove lazy loading from image shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -31,7 +31,6 @@ async function imageShortcode(src, alt, sizes = "100vw") {
         width="${lowsrc.width}"
         height="${lowsrc.height}"
         alt="${alt}"
-        loading="lazy"
         decoding="async">
     </picture>`;
 }
